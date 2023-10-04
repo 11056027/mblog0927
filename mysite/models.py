5a281276e5a590c7cd3788d72a989cebcbf5982e
11056027 <11056027@ntub.edu.tw>
2023-10-04 15:51:57 +0800

    新增Post跟管理物件

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200) #字元長度
+    slug = models.CharField(max_length=200)
+    body = models.TextField() #不限制長度
+    pub_date = models.DateTimeField(auto_now_add=True) #auto_now自動取得現在時間
+    
+    class Meta:
+        ordering = ('-pub_date',) #ordering用-pub_date做排序
+        
+    def __str__(self) -> str:
+        return self.title
